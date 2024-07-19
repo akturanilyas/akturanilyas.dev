7a768bf3011d3a239d63d4de1cebf3517ccfe773
Ilyas AKTURAN <akturanilyas@gmail.com>
2024-07-20 01:14:42 +0300

    Update dependency version

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,12 +3,14 @@ import { ProfileSection } from '@/components/home/ProfileSection';
 import { ProjectSection } from '@/components/home/ProjectSection';
 import { ExperienceSection } from '@/components/home/ExperienceSection';
 import { ErrorBoundary } from 'next/dist/client/components/error-boundary';
+import { Button } from '@akturanilyas/ui/button';
 
 export const revalidate = 604800;
 
 export default function Home() {
   return (
     <BaseView className={'h-full justify-center gap-8 my-24'}>
+        <Button>ajsbdnjsasd</Button>
       <ErrorBoundary errorComponent={undefined}>
         <ProfileSection />
       </ErrorBoundary>
